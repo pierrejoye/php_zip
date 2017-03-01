38eca285c459a4ff7d7a64b5a67681b75ab08243
Remi Collet <fedora@famillecollet.com>
2017-03-01 15:12:36 +0100

    improve encryption example (per file or global password)

--- examples/encryption.php
+++ examples/encryption.php
@@ -8,12 +8,21 @@ $name = __DIR__ . '/encrypted.zip';
 $pass = 'secret';
 $file = 'foo.php';
 
-echo "== Create\n";
+echo "== Create with per file password\n";
 
 $zip  = new ZipArchive;
 $zip->open($name, ZIPARCHIVE::CREATE | ZipArchive::OVERWRITE);
 $zip->addFile(__FILE__, $file);
 $zip->setEncryptionName($file, ZipArchive::EM_AES_256, $pass);
+$zip->close();
+
+echo "== Create with global password\n";
+
+$zip  = new ZipArchive;
+$zip->open($name, ZIPARCHIVE::CREATE | ZipArchive::OVERWRITE);
+$zip->setPassword($pass);
+$zip->addFile(__FILE__, $file);
+$zip->setEncryptionName($file, ZipArchive::EM_AES_256);
 $zip->close();
 
 echo "== Stat\n";
