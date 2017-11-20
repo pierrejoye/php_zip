2b1fc3d1446e54775d91fc3f9525372026aae7b3
Remi Collet <remi@remirepo.net>
2017-11-20 09:37:46 +0100

    Better fix for 75540 - only 1.3.1 is affected (fixed upstream) - fix use after free

--- php5/php_zip.c
+++ php5/php_zip.c
@@ -1675,9 +1675,10 @@ static ZIPARCHIVE_METHOD(close)
 	ze_obj = (ze_zip_object*) zend_object_store_get_object(this TSRMLS_CC);
 
 	if ((err = zip_close(intern))) {
+#if LIBZIP_VERSION_MAJOR == 1 && LIBZIP_VERSION_MINOR == 3 && LIBZIP_VERSION_MICRO == 1
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s", "zip_close have failed");
+#else
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s", zip_strerror(intern));
-#if LIBZIP_VERSION_MAJOR < 1 || (LIBZIP_VERSION_MAJOR == 1 && LIBZIP_VERSION_MINOR < 3) || (LIBZIP_VERSION_MAJOR == 1 && LIBZIP_VERSION_MINOR == 3 && LIBZIP_VERSION_MICRO < 1)
-		/* Fix memory leak in libzip < 1.3.1 */
 		zip_discard(intern);
 #endif
 	}
