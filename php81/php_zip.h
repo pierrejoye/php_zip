2de714ed3b8b745f1ccbc3a7e90046517c233045
Remi Collet <remi@remirepo.net>
2023-06-29 15:39:33 +0200

    Release 1.22.1

--- php81/php_zip.h
+++ php81/php_zip.h
@@ -34,7 +34,7 @@ extern zend_module_entry zip_module_entry;
 /* Additionnal flags not from libzip */
 #define ZIP_FL_OPEN_FILE_NOW (1u<<30)
 
-#define PHP_ZIP_VERSION "1.22.1-dev"
+#define PHP_ZIP_VERSION "1.22.1"
 
 #ifdef HAVE_LIBZIP_VERSION
 #define LIBZIP_VERSION_STR zip_libzip_version()
